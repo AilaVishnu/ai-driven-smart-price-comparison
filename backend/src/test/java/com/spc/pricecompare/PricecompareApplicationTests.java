56a8853917573b9d92997280673afbe0a1ea3203
Ailavishnu <ailavishnu2005@gmail.com>
2026-08-21 17:10:02 +0530

    test: application context loads and migrations apply

---
+++ backend/src/test/java/com/spc/pricecompare/PricecompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spc.pricecompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricecompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
