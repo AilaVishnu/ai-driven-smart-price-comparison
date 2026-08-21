3f3c697590b2a87736a183891f2d49e7e55e8d2b
Ailavishnu <ailavishnu2005@gmail.com>
2026-08-21 17:09:51 +0530

    feat(repo): user repository

---
+++ backend/src/main/java/com/spc/pricecompare/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spc.pricecompare.repository;
+
+import com.spc.pricecompare.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+}
