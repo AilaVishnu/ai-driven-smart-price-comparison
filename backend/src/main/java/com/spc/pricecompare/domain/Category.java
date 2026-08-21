633d676eeee70193a765bcec2ccd7072f2248c60
Ailavishnu <ailavishnu2005@gmail.com>
2026-08-21 17:09:50 +0530

    feat(domain): category entity

---
+++ backend/src/main/java/com/spc/pricecompare/domain/Category.java
@@ -0,0 +1,24 @@
+package com.spc.pricecompare.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String slug;
+}
